d28362c3982c1f99ef53cbaf609dd2d7987c53d8
TzuChanChuang <alice20041025@yahoo.com.tw>
2017-06-01 20:08:31 +0800

    data

--- FCN/data.py
+++ FCN/data.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import cv2
 
-data_path = 'data/'
+data_path = 'data_ori/'
 
 image_rows = 280
 image_cols = 320
@@ -13,50 +13,69 @@ image_cols = 320
 def create_train_data():
     train_data_path = os.path.join(data_path, 'train')
     images = os.listdir(train_data_path)
-    total = len(images) #24000 images
+    images.sort();
+    total = len(images) #12000 images
     imgs = np.ndarray((total, 1, image_rows, image_cols), dtype=np.uint8)
-    
-    train_label = np.loadtxt("data/train_label.csv", delimiter=",")
 
-    i = 0
+    train_mask_data_path = os.path.join(data_path, 'train_mask')
+    images_mask = os.listdir(train_mask_data_path)
+    images_mask.sort();
+    imgs_mask = np.ndarray((total, 1, image_rows, image_cols), dtype=np.uint8)
+
+    
     print('-'*30)
     print('Creating training images...')
     print('-'*30)
+
+    
+    i = 0
     for image_name in images:
         img = cv2.imread(os.path.join(train_data_path, image_name), cv2.IMREAD_GRAYSCALE)
-        img = np.array([img])
-        imgs[i] = img
+    img = np.array([img])
+    imgs[i] = img
 
         if i % 100 == 0:
             print('Done: {0}/{1} images'.format(i, total))
-        i += 1
-    print('Loading done.')
+    
+    i += 1
+
+    i = 0
+    for image_name in images_mask:
+    img_mask = cv2.imread(os.path.join(train_mask_data_path, image_name), cv2.IMREAD_GRAYSCALE)
+    img_mask = np.array([img_mask])
+        imgs_mask[i] = img_mask
+
+        if i % 100 == 0:
+            print('Done: {0}/{1} images'.format(i, total))
+    
+    i += 1
+    
 
     np.save('imgs_train.npy', imgs)
-    np.save('imgs_train_label.npy', train_label)
+    np.save('imgs_mask_train.npy', imgs_mask)
     print('Saving to .npy files done.')
 
 
 def load_train_data():  
     imgs_train = np.load('imgs_train.npy')
-    imgs_train_label = np.load('imgs_train_label.npy')
-    return imgs_train, imgs_train_label
+    imgs_mask_train = np.load('imgs_mask_train.npy')
+    return imgs_train, imgs_mask_train
 
 
-def create_test_data(): 
+def create_test_data():
     train_data_path = os.path.join(data_path, 'test')
     images = os.listdir(train_data_path)
+    images.sort()
     total = len(images)
-
     imgs = np.ndarray((total, 1, image_rows, image_cols), dtype=np.uint8)
 
     i = 0
     print('-'*30)
     print('Creating test images...')
     print('-'*30)
-    for image_name in images:
-        img = cv2.imread(os.path.join(train_data_path, image_name), cv2.IMREAD_GRAYSCALE)
 
+    for image_name in images:
+        img = cv2.imread(os.path.join(train_data_path,image_name), cv2.IMREAD_GRAYSCALE)
         img = np.array([img])
         imgs[i] = img
 
